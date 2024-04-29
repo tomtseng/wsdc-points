50c0055c45123da3a948d81da1887799dc805aa8
Tom Tseng <tom.hm.tseng@gmail.com>
2024-04-28 23:08:33 -0700

    Clean up scraper

--- scrape-data.py
+++ scrape-data.py
@@ -1,12 +1,11 @@
-# Sends POST request to https://points.worldsdc.com/lookup2020/find
-# with data "num: 23137" and prints the response.
-
 import json
 import pickle
 
 import requests
 from tqdm import tqdm
 
+# This URL and the data format were found by inspecting the network traffic when
+# I manually searched for a dancer's points on the WSDC points registry.
 url = "https://points.worldsdc.com/lookup2020/find"
 
 try:
@@ -14,16 +13,17 @@ try:
 except FileNotFoundError:
     responses = []
 last_id = responses[-1]["dancer_wsdcid"] if len(responses) > 0 else 0
-print(last_id)
-# Max ID is 23454 as of 4/28/2024, which I found by trial and error on the WSDC
-# website.
+# Max ID is 23454 as of 4/28/2024, which I found by manual trial and error,
+# i.e., by typing numbers into the WSDC points registry until I consistently got
+# no hits.
+# This runs at a rate of 4–5 requests per second on my laptop, maybe can speed
+# this up by firing multiple requests in parallel.
 for i in tqdm(range(last_id, 23455)):
     data = {"num": i}
     response = requests.post(url, data=data)
     if response.status_code == 200:
         responses.append(json.loads(response.text))
-    # Periodically save the responses just in case things break or I get
-    # rate-limited.
+    # Periodically save the responses just in case.
     if i % 100 == 0:
         pickle.dump(responses, open("data.pkl", "wb"))
 pickle.dump(responses, open("data.pkl", "wb"))
